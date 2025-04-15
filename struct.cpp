c366d494175d4cc12104e10a811c6c588d446a44
(faizsltn) <(faizsulthondaudm@gmail.com)>
2025-04-15 12:15:58 +0700

    membuat struct dan menampilkan data

--- struct.cpp
+++ struct.cpp
@@ -1,2 +1,32 @@
 #include <iostream>
 using namespace std;
+
+struct mahasiswa
+{
+    string nim;
+    string nama;
+    string alamat;
+
+};
+
+int main(){
+    mahasiswa mhs;
+
+    mhs.nim = "20240140258";
+    mhs.nama = "Faiz Sulthon";
+    mhs.alamat = "Kasihan";
+
+    cout << "Masukkan NIM = ";
+    cin >> mhs.nim;
+    cout << "Masukkan NAMA = ";
+    cin >> mhs.nama;
+    cout << "Masukkan Alamat = ";
+    cin >> mhs.alamat;
+
+    //menampilkan data struct
+    cout << endl;
+    cout << "NIM = " << mhs.nim << endl;
+    cout << "Nama = " << mhs.nama << endl;
+    cout << "Alamat = " << mhs.alamat << endl;
+
+}
